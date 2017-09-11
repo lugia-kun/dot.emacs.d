3050858a6a6f1f132e91050482b4cf5934719288
Hajime Yoshimori <lugia.kun@gmail.com>
2017-09-11 22:58:29 +0900

    Add font settings for macOS.

--- custom/setup-fonts.el
+++ custom/setup-fonts.el
@@ -1,17 +1,27 @@
 
-(if (not (or (eql window-system 'w32) (eql window-system 'x)))
-    nil
-    (defun my-set-font () "Set font"
-           (set-default-font "Ubuntu Mono 12")
-           ;; (set-fontset-font (frame-parameter nil 'font)
-           ;;                   'japanese-jisx0208
-           ;;                   '("MeiryoKe_Console" . "unicode-bmp")
-           ;;                   )
-           ;; (set-fontset-font (frame-parameter nil 'font)
-           ;;                   'katakana-jisx0201
-           ;;                   '("MeiryoKe_Console" . "unicode-bmp")
-           ;;                   )
-           )
-    (my-set-font))
+(defun my-set-font () "Set font"
+       (if (not (or (eql window-system 'w32) (eql window-system 'x)))
+           (if (not (eql window-system 'ns))
+               ;;               (set-default-font "Source Han Code JP 12"))
+               nil
+             (set-default-font "Ubuntu Mono 16")
+             (set-fontset-font (frame-parameter nil 'font)
+                               'japanese-jisx0213.2004-1
+                               '("Source Han Code JP" . "unicode-bmp"))
+             (set-fontset-font (frame-parameter nil 'font)
+                               'katakana-sjis
+                               '("Source Han Code JP" . "unicode-bmp")))
+         (set-default-font "Ubuntu Mono 12")
+         ;; (set-fontset-font (frame-parameter nil 'font)
+         ;;                   'japanese-jisx0208
+         ;;                   '("MeiryoKe_Console" . "unicode-bmp")
+         ;;                   )
+         ;; (set-fontset-font (frame-parameter nil 'font)
+         ;;                   'katakana-jisx0201
+         ;;                   '("MeiryoKe_Console" . "unicode-bmp")
+         ;;                   )
+         ))
+
+(my-set-font)
 
 (provide 'setup-fonts)
