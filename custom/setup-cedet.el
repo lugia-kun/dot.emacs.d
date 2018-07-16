402ebcde214813cf0a856c708b46ea33e2865259
Hajime Yoshimori <lugia.kun@gmail.com>
2018-07-16 18:50:10 +0900

    Revise moo-jump-local
    - disable ggtags.

--- custom/setup-cedet.el
+++ custom/setup-cedet.el
@@ -1,14 +1,23 @@
 (require 'cc-mode)
 (require 'semantic)
 
+;; Dependent package of function-args, swiper, will be fail to build
+;; on macOS because OS-bundled version of makeinfo is too old.
+;; Install texinfo with Homebrew or Macports and set PATH of newer
+;; version of makeinfo.
+(el-get-bundle "function-args")
+(require 'function-args)
+
 (global-semanticdb-minor-mode 1)
 (global-semantic-idle-scheduler-mode 1)
 (global-semantic-stickyfunc-mode 1)
 
 (semantic-mode 1)
+(fa-config-default)
 
 (defun alexott/cedet-hook ()
-  (local-set-key "\C-c\C-j" 'semantic-ia-fast-jump)
+  (local-set-key "\C-c\C-j" 'moo-jump-local)
+  (local-set-key "\C-c\C-f" 'semantic-ia-fast-jump)
   (local-set-key "\C-c\C-s" 'semantic-ia-show-summary))
 
 (add-hook 'c-mode-common-hook 'alexott/cedet-hook)
