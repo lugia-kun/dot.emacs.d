367f98d782002f53246932e27bfbd9b17424c04f
Hajime Yoshimori <lugia.kun@gmail.com>
2018-09-16 21:20:55 +0900

    Use Ocami.

--- custom/setup-fonts.el
+++ custom/setup-fonts.el
@@ -1,19 +1,26 @@
 
 (defun my-set-font () "Set font"
        (cond ((eql window-system 'w32)
-              (set-default-font "Cica 12"))
+              (set-default-font "Ocami 12"))
              ((eql window-system 'ns)
               (progn
-                (set-default-font "Cica 16")
+                (set-default-font "Ocami 16")
                 (set-fontset-font (frame-parameter nil 'font)
                                   'japanese-jisx0213.2004-1
-                                  '("Cica" . "unicode-bmp"))
+                                  '("Ocami" . "unicode-bmp"))
                 (set-fontset-font (frame-parameter nil 'font)
                                   'katakana-sjis
-                                  '("Cica" . "unicode-bmp"))))
+                                  '("Ocami" . "unicode-bmp"))))
              ((eql window-system 'x)
               (progn
-                (set-default-font "Cica 12")))))
+                (set-default-font "Ocami 12")
+                (set-fontset-font (frame-parameter nil 'font)
+                                  'japanese-jisx0213.2004-1
+                                  '("Ocami" . "unicode-bmp"))
+                (set-fontset-font (frame-parameter nil 'font)
+                                  'katakana-sjis
+                                  '("Ocami" . "unicode-bmp"))))
+             ))
 (my-set-font)
 
 (provide 'setup-fonts)
