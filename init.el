44c7fd1cc78d4117e4cdde77223132280423bb9b
Hajime Yoshimori <lugia.kun@gmail.com>
2020-08-25 14:58:35 +0900

    Remove local setting.

--- init.el
+++ init.el
@@ -57,26 +57,3 @@
 
 (require 'setup-hydra)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(delete-selection-mode nil)
- '(package-selected-packages
-   (quote
-    (undo-tree irony robe zygospore company clean-aindent-mode bind-key)))
- '(robe-completing-read-func (quote helm-robe-completing-read))
- '(safe-local-variable-values
-   (quote
-    ((eval set
-           (make-local-variable
-            (quote my-project-path))
-           (expand-file-name
-            (locate-dominating-file default-directory ".dir-locals.el")))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
