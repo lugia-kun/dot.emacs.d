873df9e05d7a28e0f2f4406129c0f4460c830514
Hajime Yoshimori <yoshimori-hj@yse.co.jp>
2023-12-01 16:21:38 +0900

    Goodbye semantic

--- custom/setup-cedet.el
+++ custom/setup-cedet.el
@@ -1,20 +1,20 @@
 (require 'cc-mode)
-(require 'semantic)
+;;; (require 'semantic)
 
-(global-semanticdb-minor-mode 1)
-(global-semantic-idle-scheduler-mode 1)
-(global-semantic-stickyfunc-mode 1)
-
-(semantic-mode 1)
-
-(defun alexott/cedet-hook ()
-  (local-set-key "\C-c\C-j" 'moo-jump-local)
-  (local-set-key "\C-c\C-f" 'semantic-ia-fast-jump)
-  (local-set-key "\C-c\C-s" 'semantic-ia-show-summary))
-
-(add-hook 'c-mode-common-hook 'alexott/cedet-hook)
-(add-hook 'c-mode-hook 'alexott/cedet-hook)
-(add-hook 'c++-mode-hook 'alexott/cedet-hook)
+;;; (global-semanticdb-minor-mode 1)
+;;; (global-semantic-idle-scheduler-mode 1)
+;;; (global-semantic-stickyfunc-mode 1)
+;;;
+;;; (semantic-mode 1)
+;;;
+;;; (defun alexott/cedet-hook ()
+;;;   (local-set-key "\C-c\C-j" 'moo-jump-local)
+;;;   (local-set-key "\C-c\C-f" 'semantic-ia-fast-jump)
+;;;   (local-set-key "\C-c\C-s" 'semantic-ia-show-summary))
+;;;
+;;; (add-hook 'c-mode-common-hook 'alexott/cedet-hook)
+;;; (add-hook 'c-mode-hook 'alexott/cedet-hook)
+;;; (add-hook 'c++-mode-hook 'alexott/cedet-hook)
 
 ;; Enable EDE only in C/C++
 (require 'ede)
