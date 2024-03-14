215d706a18ce1d67d09ccbdecab41694857328f2
Hajime Yoshimori <yoshimori-hj@yse.co.jp>
2024-03-14 10:53:33 +0900

    Add pgtk font config

--- custom/setup-fonts.el
+++ custom/setup-fonts.el
@@ -3,23 +3,12 @@
 
 (defun my-set-font () "Set font"
        (cl-case window-system
-         ('w32 (set-face-font 'default "Ocami 12")
-               (with-eval-after-load "markdown-mode"
-                 (set-face-font 'markdown-code-face "Ocami 12")))
-         ('ns (set-face-font 'default "Ocami 16")
-              (set-fontset-font (frame-parameter nil 'font)
-                                '(#xe000 . #xf8ff) "Ocami")
-              (set-fontset-font (frame-parameter nil 'font)
-                                'japanese-jisx0213.2004-1
-                                '("Ocami" . "unicode-bmp"))
-              (set-fontset-font (frame-parameter nil 'font)
-                                'katakana-sjis
-                                '("Ocami" . "unicode-bmp"))
+         (w32 (set-face-font 'default "Ocami 12")
               (with-eval-after-load "markdown-mode"
-                (set-face-font 'markdown-code-face "Ocami 16")))
-         ('x (set-face-font 'default "Ocami 12")
+                (set-face-font 'markdown-code-face "Ocami 12")))
+         (ns (set-face-font 'default "Ocami 16")
              (set-fontset-font (frame-parameter nil 'font)
-                               '(#xe000 . #xf8ff) "Ocami 12")
+                               '(#xe000 . #xf8ff) "Ocami")
              (set-fontset-font (frame-parameter nil 'font)
                                'japanese-jisx0213.2004-1
                                '("Ocami" . "unicode-bmp"))
@@ -27,7 +16,33 @@
                                'katakana-sjis
                                '("Ocami" . "unicode-bmp"))
              (with-eval-after-load "markdown-mode"
-               (set-face-font 'markdown-code-face "Ocami")))
+               (set-face-font 'markdown-code-face "Ocami 16")))
+         (x (set-frame-font "Ocami 12" nil t)
+            (set-fontset-font (frame-parameter nil 'font)
+                              '(#xe000 . #xf8ff) "Ocami 12")
+            (set-fontset-font (frame-parameter nil 'font)
+                              'japanese-jisx0213.2004-1
+                              '("Ocami" . "unicode-bmp"))
+            (set-fontset-font (frame-parameter nil 'font)
+                              'katakana-sjis
+                              '("Ocami" . "unicode-bmp"))
+            (with-eval-after-load "markdown-mode"
+              (set-face-font 'markdown-inline-code-face "Ocami")
+              (set-face-font 'markdown-pre-face "Ocami")
+              (set-face-font 'markdown-markup-face "Ocami")))
+         (pgtk (set-frame-font "Ocami 12" nil t)
+               (set-fontset-font (frame-parameter nil 'font)
+                                 '(#xe000 . #xf8ff) "Ocami 12")
+               (set-fontset-font (frame-parameter nil 'font)
+                                 'japanese-jisx0213.2004-1
+                                 '("Ocami" . "unicode-bmp"))
+               (set-fontset-font (frame-parameter nil 'font)
+                                 'katakana-sjis
+                                 '("Ocami" . "unicode-bmp"))
+               (with-eval-after-load "markdown-mode"
+                 (set-face-font 'markdown-inline-code-face "Ocami")
+                 (set-face-font 'markdown-pre-face "Ocami")
+                 (set-face-font 'markdown-markup-face "Ocami")))
          ))
 
 (my-set-font)
